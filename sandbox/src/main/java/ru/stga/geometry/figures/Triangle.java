8a25a0cc0a9c17071a94cc483ccfe829214d0983
Veranika Los <losveronik@gmail.com>
2024-02-11 17:27:41 +0100

    Задание №2

---
+++ sandbox/src/main/java/ru/stga/geometry/figures/Triangle.java
@@ -0,0 +1,24 @@
+package ru.stga.geometry.figures;
+
+public class Triangle {
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double area() {
+        var p = (a + b + c) / 2;
+        var d = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return d;
+    }
+
+    public double perimeter() {
+        var a = this.a + this.b + this.c;
+        return a;
+    }
+}
---
+++ sandbox/src/test/java/ru/stga/geometry/figures/SquareTests.java
@@ -0,0 +1,20 @@
+package ru.stga.geometry.figures;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SquareTests {
+
+    @Test
+     void canCalculateArea() {
+        Assertions.assertEquals(12.0, new Triangle(5.0, 6.0, 5.0).area());
+        Assertions.assertEquals(14.696938456699069, new Triangle(5.0, 6.0, 7.0).area());
+        Assertions.assertEquals(10.825317547305483, new Triangle(5.0, 5.0, 5.0).area());
+    }
+
+    @Test
+    void canCalculatePerimeter() {
+        Assertions.assertEquals(16.0, new Triangle(5.0, 6.0, 5.0).perimeter());
+        Assertions.assertEquals(18.0, new Triangle(5.0, 6.0, 7.0).perimeter());
+        Assertions.assertEquals(15.0, new Triangle(5.0, 5.0, 5.0).perimeter());
+    }
+}
